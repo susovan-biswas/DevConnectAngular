0d3655a1aa5ae2e08486dd1198f86e590889c49b
Susovan Biswas <susovan.asp@gmail.com>
2023-07-05 18:49:43 +0530

    End of section 14

---
+++ API/DTOs/LikeDto.cs
@@ -0,0 +1,11 @@
+namespace API.DTOs
+{
+    public class LikeDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string City { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
